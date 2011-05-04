e9c9ba1a9f70a723a953ae23d09363b3cbe0564a
Tim 'mithro' Ansell <mithro@mithis.com>
2011-05-04 21:21:20 +1000

    Flumotion configurations.

--- amazon/setupserver.sh
+++ amazon/setupserver.sh
@@ -3,6 +3,7 @@
 source common.sh
 
 # Setup a connection
+ssh $USER@$SERVER true
 rm /home/tansell/.ssh/tmp/master-$USER@$SERVER:22
 ssh $USER@$SERVER sleep 300 >/dev/null 2>&1 < /dev/null &
 SSHPID=$!
@@ -15,30 +16,22 @@ while [ $CONNECTED -ne 1 ]; do
 	CONNECTED=$?
 done
 
-# Add multiverse
-$SUDO "cat /etc/apt/sources.list | sed -e's/main universe/multiverse/' > /tmp/multiverse.list"
+$SUDO "apt-get -f install"
+
+# Enable multiverse
+$SUDO "cat /etc/apt/sources.list | grep 'universe' | sed -e's/universe/multiverse/' > /tmp/multiverse.list"
 $SUDO "mv /tmp/multiverse.list /etc/apt/sources.list.d/multiverse.list"
 
-# Make sure everything is configured
-$SUDO "dpkg --configure -a"
+# Add the required fluendo PPAs
+$SUDO "apt-add-repository ppa:flumotion-dev/flumotion"
+#$SUDO "apt-add-repository ppa:gstreamer-developers/ppa"
 
-# Clean up any hanging files
-$SUDO "apt-get autoremove"
+$SUDO "rm /etc/apt/sources.list.d/*.save"
 
 # Update the software
 $SUDO "aptitude update"
 $SUDO "aptitude full-upgrade -y"
 
-# Install ffmpeg
-$SUDO "dpkg --remove ffmpeg"
-scp ../debs/ffmpeg/* $USER@$SERVER:/tmp
-$SUDO "dpkg --install /tmp/*.deb"
-$SUDO "rm /tmp/*.deb"
-
-# Install any deps ffmpeg needs
-$SUDO "apt-get install -y libxvidcore4 libvorbisenc2 libtheora0 libopencore-amrwb0 libopencore-amrnb0 libfaac0 libxfixes3 libasound2 libx264-85"
-$SUDO "apt-get -f install"
-
-. updateconfig.sh $1 $2 $3
+$SUDO "apt-get install -y flumotion flumotion-ugly"
 
 kill $SSHPID
