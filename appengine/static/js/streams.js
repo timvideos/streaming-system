bc09b2a38ac95ffc6c6a5289d21e3fa9398be5f8
Tim 'mithro' Ansell <mithro@mithis.com>
2011-06-28 20:10:29 +1000

    Making HTML5 the fallback rather then primary.

--- appengine/static/js/streams.js
+++ appengine/static/js/streams.js
@@ -84,20 +84,20 @@ function jwplayer_modes(format, quality) {
   if (format == 'auto') {
     var modes = [];
 
-    var html5_modes = jwplayer_modes('html5', quality);
-    for (i in html5_modes) {
-      if (html5_modes[i].type == 'download') {
-        download = html5_modes[i].download;      
-        continue;
-      }
-      modes.push(html5_modes[i]);
-    }
     var flash_modes = jwplayer_modes('flash', quality);
     for (i in flash_modes) {
       if (flash_modes[i].type == 'download') {
         continue;
       }
       modes.push(flash_modes[i]);
+    }
+    var html5_modes = jwplayer_modes('html5', quality);
+    for (i in html5_modes) {
+      if (html5_modes[i].type == 'download') {
+        download = html5_modes[i].download;
+        continue;
+      }
+      modes.push(html5_modes[i]);
     }
   } else {
     var stream = jwplayer_streams(format);
