d69354530733d1771ca797607ce68616ff26c7a6
Tim Ansell <mithro@mithis.com>
2011-08-30 13:34:14 +1000

    Remove any cache settings.

--- tools/setup/05-update.sh
+++ tools/setup/05-update.sh
@@ -6,6 +6,8 @@
 # Updates the machine to the latest version of every installed package.
 #
 
+rm /etc/apt/apt.conf.d/02cache
+
 # Remove any duplicate .save files which end up in /etc/apt/sources.list.d from
 # installing PPAs multiple times. This prevents warning about duplicate sources.
 rm /etc/apt/sources.list.d/*.save
