e0ea64c87eb336cc07fdb6253796e1f71ee0dd8a
Tim 'mithro' Ansell <mithro@mithis.com>
2012-01-10 22:07:22 +1100

    Making the camera page also work.

--- tools/portable/portable.py
+++ tools/portable/portable.py
@@ -37,6 +37,7 @@ class SetUpPage(object):
         self.xml = xml
 
         self.page = xml.get_page(self.xmlname)
+
         self.index = assistant.append_page(self.page)
         assistant.set_page_title(self.page, self.title)
         assistant.set_page_type(self.page, gtk.ASSISTANT_PAGE_CONTENT)
@@ -47,10 +48,17 @@ class SetUpPage(object):
 
     def on_prepare(self, assistant, page):
         if page == self.page:
+            print "on_prepare", self
             self.timer = gobject.timeout_add(self.interval, self.update)
-            self.update()
+            self.on_show()
         else:
-            self.timer = None
+            self.on_unprepare()
+
+    def on_unprepare(self):
+        self.timer = None
+
+    def on_show(self):
+        self.update()
 
 
 class BatteryPage(SetUpPage):
@@ -93,48 +101,62 @@ class NetworkPage(SetUpPage):
         return False
 
 
-class PresentationPage(SetUpPage):
-    xmlname = "presentation"
-    title = "Presentation Capture Setup"
+class VideoPage(SetUpPage):
 
     def __init__(self, *args, **kw):
         self.player = None
         SetUpPage.__init__(self, *args, **kw)
 
+        video = self.xml.get_object(self.video_component)
+        video.unset_flags(gtk.DOUBLE_BUFFERED)
+        video.connect("expose-event", self.on_expose)
+        video.connect("map-event", self.on_map)
+        video.connect("unmap-event", self.on_unprepare)
+
     def update(self, evt=None):
         if self.timer is not None:
+            print self, "update"
             if self.player is not None:
-                print self.player.get_state()[1], gst.STATE_PLAYING
-                if gst.STATE_PLAYING != self.player.get_state()[1]:
-                    self.player.set_state(gst.STATE_PLAYING)
-                else:
+                print "Getting player state"
+                if (gst.STATE_CHANGE_SUCCESS, gst.STATE_PLAYING) == self.player.get_state(timeout=1)[:-1]:
+                    print "After"
                     self.assistant.set_page_complete(self.page, True)
                     return True
-            else:
-                self.create_video()
+                print "After"
 
             self.assistant.set_page_complete(self.page, False)
             return True
 
         return False
 
-    def create_video(self):
-        # Set up the gstreamer pipeline
-        # FIXME: Need to keep this in sync with the flumotion-config/collector-portable.xml
-        self.player = gst.parse_launch("""\
-v4l2src !
-videocrop left=80 right=80 top=0 bottom=0 !
-videoscale !
-autovideosink
-""")
+    def on_expose(self, *args):
+        video = self.xml.get_object(self.video_component)
+        # Force the window to be realized
+        video.window.xid
 
+    def on_map(self, *args):
+        video = self.xml.get_object(self.video_component)
+        self.video_xid = video.window.xid
+
+        self.player = gst.parse_launch(self.video_pipeline)
         bus = self.player.get_bus()
         bus.add_signal_watch()
         bus.enable_sync_message_emission()
         bus.connect("message", self.on_message)
         bus.connect("sync-message::element", self.on_sync_message)
 
+        self.player.set_state(gst.STATE_PLAYING)
+
+    def on_unprepare(self, *args):
+        if self.player is not None:
+            self.player.set_state(gst.STATE_NULL)
+            while (gst.STATE_CHANGE_SUCCESS, gst.STATE_NULL) != self.player.get_state()[:-1]:
+                pass
+            sys.stdout.flush()
+            self.player = None
+
     def on_message(self, bus, message):
+        print message
         t = message.type
         if t == gst.MESSAGE_EOS:
             print "End of stream!?"
@@ -146,12 +168,48 @@ autovideosink
             self.player.set_state(gst.STATE_NULL)
 
     def on_sync_message(self, bus, message):
+        print message
         if message.structure is None:
             return
         message_name = message.structure.get_name()
         if message_name == "prepare-xwindow-id":
             message.src.set_property("force-aspect-ratio", True)
-            message.src.set_xwindow_id(self.xml.get_object('presentation-preview').window.xid)
+            message.src.set_xwindow_id(self.video_xid)
+
+
+class PresentationPage(VideoPage):
+    xmlname = "presentation"
+    title = "Presentation Capture Setup"
+
+    # FIXME: Need to keep this in sync with producer-firewire in flumotion-config/collector-portable.xml
+    video_pipeline = """\
+v4l2src device=/dev/video1 !
+image/jpeg,width=640,height=480,framerate=(fraction)24/1 !
+jpegdec !
+videocrop left=80 right=80 top=0 bottom=0 !
+videoscale !
+autovideosink
+"""
+    video_component = 'presentation-preview'
+
+
+class CameraPage(VideoPage):
+    xmlname = "camera"
+    title = "Presenter Capture Setup"
+
+    # FIXME: Need to keep this in sync with composite-video the flumotion-config/collector-portable.xml
+    video_pipeline = """\
+v4l2src device=/dev/video1 !
+image/jpeg,width=640,height=480,framerate=(fraction)24/1 !
+jpegdec !
+videocrop left=80 right=80 top=0 bottom=0 !
+videoscale !
+autovideosink
+"""
+    video_component = 'camera-preview'
+
+
+
 
 
 class App(object):
@@ -188,29 +246,24 @@ class App(object):
         battery = BatteryPage(assistant, xml)
         network = NetworkPage(assistant, xml)
         presentation = PresentationPage(assistant, xml)
-
-        camera = xml.get_page("camera")
-        assistant.append_page(camera)
-        assistant.set_page_title(camera, "Camera Capture Setup")
-        assistant.set_page_type(camera, gtk.ASSISTANT_PAGE_PROGRESS)
-        assistant.set_page_complete(camera, False)
+        camera = CameraPage(assistant, xml)
 
         interaction = xml.get_page("interaction")
         assistant.append_page(interaction)
         assistant.set_page_title(interaction, "Interaction Setup")
-        assistant.set_page_type(interaction, gtk.ASSISTANT_PAGE_PROGRESS)
+        assistant.set_page_type(interaction, gtk.ASSISTANT_PAGE_CONTENT)
         assistant.set_page_complete(interaction, False)
 
         audio_inroom = xml.get_page("audio-inroom")
         assistant.append_page(audio_inroom)
         assistant.set_page_title(audio_inroom, "Inroom Audio Setup")
-        assistant.set_page_type(audio_inroom, gtk.ASSISTANT_PAGE_PROGRESS)
+        assistant.set_page_type(audio_inroom, gtk.ASSISTANT_PAGE_CONTENT)
         assistant.set_page_complete(audio_inroom, False)
 
         audio_standalone = xml.get_page("audio-standalone")
         assistant.append_page(audio_standalone)
         assistant.set_page_title(audio_standalone, "Standalone Audio Setup")
-        assistant.set_page_type(audio_standalone, gtk.ASSISTANT_PAGE_PROGRESS)
+        assistant.set_page_type(audio_standalone, gtk.ASSISTANT_PAGE_CONTENT)
         assistant.set_page_complete(audio_standalone, False)
 
         # and the window
