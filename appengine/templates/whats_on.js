5166c9ab76230d75dd3cdb4bc9619fab245cc4fc
Tim 'mithro' Ansell <mithro@mithis.com>
2012-01-16 23:57:01 +1100

    Fixing whats_on.js

--- appengine/templates/whats_on.js
+++ appengine/templates/whats_on.js
@@ -1,11 +1,10 @@
 // -*- coding: utf-8 -*-
 // vim: set ts=2 sw=2 et sts=2 ai:
 
-var time_local = new Date();
-var time_server = new Date({{current_time}});
-var time_offset = 39600000; // 11 hours
+var time_local = Date.now();
+var time_server = {{current_time}};
 function current_time() {
-  return new Date((new Date() - (time_local-time_server)) + time_offset);
+  return Date.now() - (time_local-time_server);
 }
 
 var schedule = [];
@@ -32,7 +31,7 @@ function current_session(group) {
         continue;
       }
 
-    var start = Date.parse(talk['Start'] += " GMT+1100 (EST)");
+    var start = Date.parse(talk['Start'] += " GMT") + 39600000;
     var duration_bits = talk['Duration'].split(':');
     var duration = parseInt(duration_bits[0])*60*60 + parseInt(duration_bits[1])*60 + parseInt(duration_bits[2]);
     var end = start + duration*1e3;
