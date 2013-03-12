52afde19d2c13636523366439e7b169ef9a6401e
Tim 'mithro' Ansell <mithro@mithis.com>
2013-03-12 16:39:39 -0700

    Fixing the settings.

--- website/settings.py
+++ website/settings.py
@@ -56,7 +56,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/tmp/timsvideo-static'
+STATIC_ROOT = '/tmp/timvideos-static'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
