16547240a44ecb4c649f4af93bcfa706d0415f9f
Tim 'mithro' Ansell <mithro@mithis.com>
2013-03-13 22:48:02 -0700

    Another attempt.

--- tools/preview/generate.sh
+++ tools/preview/generate.sh
@@ -6,16 +6,16 @@ for GROUP in $GROUPS; do
 	mkdir /srv/\#pycon-$GROUP
 done
 
-while true; do
+for GROUP in $GROUPS; do
 	cd /srv
-	for GROUP in $GROUPS; do 
+	while true; do 
 	        (
 	         cd \#pycon-$GROUP
-		 mplayer -nocache -nosound -vo png -cache 512 -nosound -vo png http://$GROUP.encoder.timvideos.us:8081/loop.raw -frames 1
+		 mplayer -nocache -nosound -vo png -cache 512 -nosound -vo png http://$GROUP.encoder.timvideos.us:8081/loop.raw -frames 1 2>/dev/null
 	         convert -resize 300x217 00000001.png 00000001-small.png
 	         pngcrush 00000001-small.png
 	         mv 00000001-small.png latest.png
 	        )
+		sleep 1
 	done
-        sleep 1
 done
