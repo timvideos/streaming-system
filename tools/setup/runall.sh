70417e89d797c2233a6ccd3c280e20119e4d7199
Carl Karsten <carl@personnelware.com>
2013-04-27 02:35:26 -0500

    call scripts correctly

--- tools/setup/runall.sh
+++ tools/setup/runall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 
 # wget -N https://raw.github.com/timvideos/streaming-system/master/tools/setup/runall.sh
-# wget -N https://github.com/CarlFK/streaming-system/master/tools/setup/runall.sh
+# wget -N https://raw.github.com/CarlFK/streaming-system/master/tools/setup/runall.sh
 # chmod u+x runall.sh
 # ./runall.sh
 
@@ -11,11 +11,11 @@ git clone https://github.com/CarlFK/streaming-system.git
 
 cd streaming-system/tools/setup
 
-00-get-timsvideo.sh
-01-add-multiverse.sh
-01-add-streamtime-ppa.sh
-05-update.sh
-20-gstreamer-packages.sh
-20-useful-packages.sh
-30-custom-flumotion.sh
-99-remove-ppa.sh
+./00-get-timsvideo.sh
+./01-add-multiverse.sh
+./01-add-streamtime-ppa.sh
+./05-update.sh
+./20-gstreamer-packages.sh
+./20-useful-packages.sh
+./30-custom-flumotion.sh
+./99-remove-ppa.sh
