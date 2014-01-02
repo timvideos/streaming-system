22d094d5b0a53d8b143d1c5aef53d4f3258cf6d3
Tim 'mithro' Ansell <mithro@mithis.com>
2014-01-02 17:44:39 +1100

    Changing sqlite database to be inside directory so it can be writable by webserver.
    (Needed to create a lock file.)

--- website/settings.py
+++ website/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'website.sqlite3',                      # Or path to database file if using sqlite3.
+        'NAME': 'db/website.sqlite3',    # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
