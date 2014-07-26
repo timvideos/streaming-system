682dae9040d46f143f9a68221b79e09a91443b36
Tim 'mithro' Ansell <mithro@mithis.com>
2014-07-27 04:15:19 +0930

    Adding a bit more info to the urls.py

--- website/frontend/urls.py
+++ website/frontend/urls.py
@@ -9,12 +9,13 @@ from frontend import views
 from . import feeds
 
 urlpatterns = patterns('',
-    url(r'^(monitor)$', views.index),
-    url(r'^', include(feeds.urls)),
-    url(r'^(.+)/json$', views.json_feed),
-    url(r'^(.+)$', views.group),
+    # Site wide pages
     url(r'^$', views.index),
-    
-    # Endpoints for graphing.
+    url(r'^(monitor)$', views.index),
     url(r'^graphs$', views.overall_stats_graphs),
+
+    # Per room things
+    url(r'^', include(feeds.urls)),       # room/rss - Schedule as RSS
+    url(r'^(.+)/json$', views.json_feed), # room/json - Schedule as json
+    url(r'^(.+)$', views.group),          # room/ - Base page
    )
